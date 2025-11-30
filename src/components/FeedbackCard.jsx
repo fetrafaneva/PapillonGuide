9d2c3420becc1674fbe1ee53b0ba2ff29d36109c
2shining <fetrafaneva@gmail.com>
2025-11-30 09:36:54 +0300

    quelque refactoring de code

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,5 +1,3 @@
-import { quotes } from "../assets";
-
 const FeedbackCard = ({ content, name, img }) => (
   <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
     <svg
